f5ba6d5cc7e966bbc4f4d86dcaf80ad34d003228
Shikhar Kumar <shikharkumar009@gmail.com>
2022-03-12 16:47:47 +0530

    Working v1.00 Activities Created

---
+++ app/src/main/java/com/aliferous/signlanguage/SL2EngActivity.java
@@ -0,0 +1,14 @@
+package com.aliferous.signlanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SL2EngActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sl2eng);
+    }
+}
