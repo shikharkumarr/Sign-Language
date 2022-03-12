2a0654bca2ae951949ad0f9f00b459629fb99c47
Shikhar Kumar <shikharkumar009@gmail.com>
2022-03-12 17:58:29 +0530

    Working v1.03

--- app/src/main/java/com/aliferous/signlanguage/SL2EngActivity.java
+++ app/src/main/java/com/aliferous/signlanguage/SL2EngActivity.java
@@ -2,13 +2,30 @@ package com.aliferous.signlanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class SL2EngActivity extends AppCompatActivity {
+
+    FrameLayout sl2engBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sl2eng);
+
+        sl2engBTN = findViewById(R.id.sl2engBTN);
+
+        sl2engBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SL2EngActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
